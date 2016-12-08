961eba98d973802699babf344bcf0fa8aaf07d1b
marco79423 <marco79423@gmail.com>
2016-12-08 21:23:13 +0800

    Added API root

--- api/urls.py
+++ api/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import url
 from api import views
 
 urlpatterns = [
-    url(r'^articles/$', views.get_article_list),
+    url(r'^$', views.get_root),
+    url(r'^articles/$', views.get_article_list, name='articles'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,8 +1,16 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.reverse import reverse
 
 from api.serializers import ArticleSerializer
 from app.models import Article
+
+
+@api_view(['GET'])
+def get_root(request):
+    return Response({
+        'articles': reverse('articles', request=request),
+    })
 
 
 @api_view(['GET'])
