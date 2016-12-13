98166b4858d710f45d25d1bc5582063cabde632d
marco79423 <marco79423@gmail.com>
2016-12-13 20:50:05 +0800

    修改 articles api 的格式

--- api/serializers.py
+++ api/serializers.py
@@ -1,12 +1,19 @@
-from rest_framework.serializers import ModelSerializer
 from rest_framework import serializers
+from app.models import Category
 from app.models import Article
 
 
-class ArticleSerializer(ModelSerializer):
-    categories = serializers.StringRelatedField(many=True, read_only=True)
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ('slug', 'name',)
+
+
+class ArticleSerializer(serializers.ModelSerializer):
+    categories = CategorySerializer(many=True, read_only=True)
 
     class Meta:
         model = Article
         fields = ('slug', 'title', 'date', 'modified_date', 'categories',
                   'content', 'summary', 'raw_summary', 'series',)
+
