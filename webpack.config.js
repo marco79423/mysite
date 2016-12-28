5197db134ea837b919027bbb7610c77810d28b7e
marco79423 <marco79423@gmail.com>
2016-12-28 11:18:32 +0800

    Revised code

--- webpack.config.js
+++ webpack.config.js
@@ -3,12 +3,16 @@ const path = require('path')
 
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
+const DEBUG = (process.env.NODE_ENV !== 'production')
 
-let config = {
+module.exports = {
   entry: [
     'babel-polyfill',
     './src/js/main.js'
-  ],
+  ].concat(DEBUG ? [
+    'webpack-dev-server/client?http://0.0.0.0:3000',
+    'webpack/hot/only-dev-server'
+  ] : []),
   output: {
     publicPath: '/',
     path: path.join(__dirname, 'dist'),
@@ -18,7 +22,7 @@ let config = {
     loaders: [
       {
         test: /\.jsx?$/,
-        loader: 'babel',
+        loader: DEBUG ? 'react-hot!babel' : 'babel',
         include: path.join(__dirname, 'src')
       },
       {
@@ -42,20 +46,11 @@ let config = {
       template: path.join(__dirname, 'src', 'html', 'index.html')
     }),
     new webpack.DefinePlugin({
-      'process.env' : {
+      'process.env': {
         NODE_ENV: JSON.stringify(process.env.NODE_ENV),
-        DEBUG: (process.env.NODE_ENV !== 'production')
+        DEBUG
       }
     })
-  ]
+  ],
+  devtool: DEBUG ? 'eval' : null
 }
-
-
-if (process.env.NODE_ENV !== 'production') {
-  config.entry.push('webpack-dev-server/client?http://0.0.0.0:3000')
-  config.entry.push('webpack/hot/only-dev-server')
-  config.module.loaders[0].loader = 'react-hot!babel'
-  config.devtool = 'eval'
-}
-
-module.exports = config
