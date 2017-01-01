eeaf53e6e1adebdf60f0e3468171675ef48952bc
marco79423 <marco79423@gmail.com>
2017-01-01 14:56:29 +0800

    Revised code

--- webpack.config.client.js
+++ webpack.config.client.js
@@ -47,7 +47,7 @@ module.exports = {
     new webpack.DefinePlugin({
       'process.env': {
         NODE_ENV: JSON.stringify(process.env.NODE_ENV),
-        DEBUG
+        DEBUG: DEBUG
       }
     })
   ].concat(DEBUG ? [
