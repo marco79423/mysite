4e6e16b53ea8a80c609fab0c145fe9a173333c88
marco79423 <marco79423@gmail.com>
2017-01-08 10:53:55 +0800

    Adjusted style of article

--- src/common/html-render.js
+++ src/common/html-render.js
@@ -13,17 +13,17 @@ export class Html extends React.Component {
     const state = serialize(this.props.state.toJS())
     return (
       <html lang="en">
-        <head>
-          <meta charSet="UTF-8"/>
-          <title>兩大類 x 兩大類 = 四大類</title>
-          <link href="/assets/styles/styles.css" rel="stylesheet"/>
-          <link href="/assets/styles/vendor.css" rel="stylesheet"/>
-        </head>
-        <body>
-        <div id="app" dangerouslySetInnerHTML={{__html: this.props.html}} />
-        <script dangerouslySetInnerHTML={{__html: `window.__PRELOADED_STATE__=${state}`}} charSet="UTF-8"/>
-        <script type="text/javascript" src="/assets/bundle.js"/>
-        </body>
+      <head>
+        <meta charSet="UTF-8"/>
+        <title>兩大類 x 兩大類 = 四大類</title>
+        <link href="/assets/styles/vendor.css" rel="stylesheet"/>
+        <link href="/assets/styles/styles.css" rel="stylesheet"/>
+      </head>
+      <body>
+      <div id="app" dangerouslySetInnerHTML={{__html: this.props.html}} />
+      <script dangerouslySetInnerHTML={{__html: `window.__PRELOADED_STATE__=${state}`}} charSet="UTF-8"/>
+      <script type="text/javascript" src="/assets/bundle.js"/>
+      </body>
       </html>
     )
   }
