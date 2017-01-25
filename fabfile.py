39fe7443b6e25b8e1eab2eb2788e9cf32c69d9a8
marco79423 <marco79423@gmail.com>
2017-01-25 22:07:15 +0800

    Rewrited fabfile.py

--- fabfile.py
+++ fabfile.py
@@ -1,4 +1,5 @@
 from __future__ import print_function
+import json
 from StringIO import StringIO
 
 from fabric.api import *
@@ -6,18 +7,48 @@ from fabric.colors import cyan
 from fabric.contrib.files import exists, sed
 
 
-PROJECT_PATH = '/var/www/mysite-backend'
+HOST_CONFIG_FILE = "hosts.json"
+
+CONFIGS = {
+    'prod': {
+        'name': 'mysite-backend',
+        'debug': False,
+        'server_name': 'api.marco79423.net',
+    },
+    'dev': {
+        'name': 'mysite-backend-dev',
+        'debug': True,
+        'server_name': 'api-dev.marco79423.net',
+    }
+}
+
+with open(HOST_CONFIG_FILE) as fp:
+    hosts_data = json.load(fp)
+    env.hosts = hosts_data.get('hosts', [])
+    env.key_filename = hosts_data.get('pem_file')
+
+
+@task
+def prod():
+    env.update(CONFIGS['prod'])
+    env.project_path = '/var/www/' + env.name
+
+
+@task
+def dev():
+    env.update(CONFIGS['dev'])
+    env.project_path = '/var/www/' + env.name
 
 
 @task
 def deploy(branch='master'):
     update_sys()
-    upload_proj(branch)
-    install_pkgs()
-    setup_proj()
-    test_proj()
+    _upload_proj(branch)
+    _install_pkgs()
+    _setup_proj()
+    _test_proj()
     build_content()
-    setup_serv()
+    _setup_serv()
     restart_serv()
 
 
@@ -28,13 +59,35 @@ def update_sys():
 
 
 @task
-def upload_proj(branch):
-    if exists(PROJECT_PATH):
-        sudo('rm -rf {}'.format(PROJECT_PATH))
+def build_content():
+    if not exists('/var/www/site-content'):
+        sudo('git clone https://github.com/marco79423/mysite-content.git /var/www/site-content')
+    else:
+        with cd('/var/www/site-content'):
+            sudo('git pull')
 
-    sudo('mkdir -p {}'.format(PROJECT_PATH))
+    with cd(env.project_path):
+        sudo('venv/bin/python manage.py build /var/www/site-content')
+
+
+@task
+def restart_serv():
+    sudo('service {name} restart'.format(name=env.name))
+    sudo('service nginx restart')
+
+
+#################
+# private tasks #
+#################
+
+@task
+def _upload_proj(branch):
+    if exists(env.project_path):
+        sudo('rm -rf {}'.format(env.project_path))
+
+    sudo('mkdir -p {}'.format(env.project_path))
     archive = local('git archive --format=tar {} | gzip'.format(branch), capture=True)
-    with cd(PROJECT_PATH):
+    with cd(env.project_path):
         put(StringIO(archive), 'temp.tar.gz', use_sudo=True)
         sudo('tar zxf temp.tar.gz')
         sudo('rm temp.tar.gz')
@@ -42,7 +95,7 @@ def upload_proj(branch):
 
 
 @task
-def install_pkgs():
+def _install_pkgs():
     print(cyan('Install python and associated packages...'))
     sudo('apt-get install -y python3.4')
     sudo('apt-get install -y python3-pip')
@@ -65,8 +118,8 @@ def install_pkgs():
 
 
 @task
-def setup_proj():
-    with cd(PROJECT_PATH):
+def _setup_proj():
+    with cd(env.project_path):
         sudo('virtualenv venv -p python3')
         sudo('venv/bin/pip install -r requirements.txt')
         sudo('chown -R www-data:www-data venv')
@@ -75,47 +128,42 @@ def setup_proj():
         sudo('venv/bin/python manage.py migrate')
         sudo('venv/bin/python manage.py collectstatic --noinput')
 
-        print(cyan('Changing setting for production ...'))
-        sed('mysite/settings.py', 'DEBUG = True', 'DEBUG = False', shell=True, use_sudo=True)
+        if not env.debug:
+            print(cyan('Changing setting for production ...'))
+            sed('mysite/settings.py', 'DEBUG = True', 'DEBUG = False', shell=True, use_sudo=True)
 
 
 @task
-def test_proj():
-    with cd(PROJECT_PATH):
+def _test_proj():
+    with cd(env.project_path):
         run("venv/bin/python manage.py test")
 
 
 @task
-def build_content():
-    if not exists('/var/www/site-content'):
-        sudo('git clone https://github.com/marco79423/mysite-content.git /var/www/site-content')
-    else:
-        with cd('/var/www/site-content'):
-            sudo('git pull')
-
-    with cd(PROJECT_PATH):
-        sudo('venv/bin/python manage.py build /var/www/site-content')
-
-
-@task
-def setup_serv():
+def _setup_serv():
     sudo('locale-gen zh_TW.UTF-8')
 
+    filename = env.name + '.conf'
+
     print(cyan('Setup gunicorn ...'))
-    with cd(PROJECT_PATH):
-        sudo('cp conf/init/mysite-backend.conf /etc/init')
+    config_path = '/etc/init/' + filename
+    with cd(env.project_path):
+        sudo('cp conf/init/service.conf ' + config_path)
+        sed(config_path, 'TARGET_NAME', env.name, shell=True, use_sudo=True)
 
     print(cyan('Setup nginx ...'))
     if exists('/etc/nginx/sites-enabled/default'):
         sudo('rm /etc/nginx/sites-enabled/default')
 
-    with cd(PROJECT_PATH):
-        sudo('cp conf/nginx/mysite-backend.conf /etc/nginx/sites-available')
-    if not exists('/etc/nginx/sites-enabled/mysite-backend.conf'):
-        sudo('ln -s /etc/nginx/sites-available/mysite-backend.conf mysite-backend.conf')
+    config_path = '/etc/nginx/sites-available/' + filename
+    with cd(env.project_path):
+        sudo('cp conf/nginx/nginx.conf ' + config_path)
+        sed(config_path, 'TARGET_NAME', env.name, shell=True, use_sudo=True)
+        sed(config_path, 'SERVER_NAME', env.server_name, shell=True, use_sudo=True)
+
+    with cd('/etc/nginx/sites-enabled'):
+        if exists(filename):
+            sudo('rm ' + filename)
+        sudo('ln -s {config_path} {filename}'.format(config_path=config_path, filename=filename))
 
 
-@task
-def restart_serv():
-    sudo('service mysite-backend restart')
-    sudo('service nginx restart')
