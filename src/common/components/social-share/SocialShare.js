620c82c658983d5c4c479de03cf97e91f23278a7
marco79423 <marco79423@gmail.com>
2017-03-07 22:19:32 +0800

    Added new share buttons (Twitter, Linkedin)

--- src/common/components/social-share/SocialShare.js
+++ src/common/components/social-share/SocialShare.js
@@ -7,11 +7,15 @@ import styles from './SocialShare.scss'
 
 const {
   FacebookShareButton,
-  GooglePlusShareButton
+  GooglePlusShareButton,
+  LinkedinShareButton,
+  TwitterShareButton
 } = ShareButtons;
 
 const FacebookIcon = generateShareIcon('facebook')
 const GooglePlusIcon = generateShareIcon('google')
+const LinkedinIcon  = generateShareIcon('linkedin')
+const TwitterIcon  = generateShareIcon('twitter')
 
 export default class SocialShare extends React.Component {
   static PropTypes = {
@@ -22,6 +26,8 @@ export default class SocialShare extends React.Component {
   }
 
   render() {
+    const iconSize = 32
+
     return (
       <div className={styles.root}>
         <FacebookShareButton
@@ -29,7 +35,7 @@ export default class SocialShare extends React.Component {
           title={this.props.config.get('title')}
           className={styles.item}>
           <FacebookIcon
-            size={32}
+            size={iconSize}
             round />
         </FacebookShareButton>
 
@@ -37,9 +43,27 @@ export default class SocialShare extends React.Component {
           url={this.props.config.get('shareUrl')}
           className={styles.item}>
           <GooglePlusIcon
-            size={32}
+            size={iconSize}
             round />
         </GooglePlusShareButton>
+
+        <TwitterShareButton
+          url={this.props.config.get('shareUrl')}
+          title={this.props.config.get('title')}
+          className={styles.item}>
+          <TwitterIcon
+            size={iconSize}
+            round />
+        </TwitterShareButton>
+
+        <LinkedinShareButton
+          url={this.props.config.get('shareUrl')}
+          title={this.props.config.get('title')}
+          className={styles.item}>
+          <LinkedinIcon
+            size={iconSize}
+            round />
+        </LinkedinShareButton>
       </div>
     )
   }
