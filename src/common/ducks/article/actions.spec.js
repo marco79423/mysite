93a9b45839e9dd734935ba5676a78d3b77cfa265
marco79423 <marco79423@gmail.com>
2017-03-25 20:53:44 +0800

    Revert to last way to write tests

--- src/common/ducks/article/actions.spec.js
+++ src/common/ducks/article/actions.spec.js
@@ -12,65 +12,62 @@ import * as actions from './actions'
 const middlewares = [thunk]
 const mockStore = configureStore(middlewares)
 
-describe('article.action', () => {
+const articles = [
+  {
+    'title': '美女最變態',
+    'date': '2013-02-02T00:00:00'
+  },
+  {
+    'title': '小雞跳樓梯',
+    'date': '2013-02-18T00:00:00',
+  },
+  {
+    'title': '擁抱',
+    'date': '2013-02-16T00:00:00',
+  }
+]
 
-  const articles = [
-    {
-      'title': '美女最變態',
-      'date': '2013-02-02T00:00:00'
-    },
-    {
-      'title': '小雞跳樓梯',
-      'date': '2013-02-18T00:00:00',
-    },
-    {
-      'title': '擁抱',
-      'date': '2013-02-16T00:00:00',
+const sortedArticles = [
+  {
+    'title': '小雞跳樓梯',
+    'date': '2013-02-18T00:00:00',
+  },
+  {
+    'title': '擁抱',
+    'date': '2013-02-16T00:00:00',
+  },
+  {
+    'title': '美女最變態',
+    'date': '2013-02-02T00:00:00'
+  }
+]
+
+test('setArticles should create an action to set articles', () => {
+  const expectedAction = {
+    type: actionTypes.SET_ARTICLES,
+    payload: articles
+  }
+  expect(actions.setArticles(articles)).toEqual(expectedAction)
+})
+
+test('fetchArticles should create an action to fetch articles for api server', done => {
+  const store = mockStore(Immutable.fromJS({
+    config: {
+      API_SERVER_URL: 'https://api.marco79423.net'
     }
-  ]
+  }))
 
-  const sortedArticles = [
+  const expectedActions = [
     {
-      'title': '小雞跳樓梯',
-      'date': '2013-02-18T00:00:00',
-    },
-    {
-      'title': '擁抱',
-      'date': '2013-02-16T00:00:00',
-    },
-    {
-      'title': '美女最變態',
-      'date': '2013-02-02T00:00:00'
-    }
-  ]
-
-  test('handle setArticles action', () => {
-    const expectedAction = {
       type: actionTypes.SET_ARTICLES,
-      payload: articles
+      payload: sortedArticles
     }
-    expect(actions.setArticles(articles)).toEqual(expectedAction)
-  })
+  ]
 
-  test('handle fetchArticles action', done => {
-    const store = mockStore(Immutable.fromJS({
-      config: {
-        API_SERVER_URL: 'https://api.marco79423.net'
-      }
-    }))
-
-    const expectedActions = [
-      {
-        type: actionTypes.SET_ARTICLES,
-        payload: sortedArticles
-      }
-    ]
-
-    fetchMock.once('https://api.marco79423.net/articles/', articles)
-    store.dispatch(actions.fetchArticles())
-      .then(() => {
-        expect(store.getActions()).toEqual(expectedActions)
-        done()
-      })
-  })
+  fetchMock.once('https://api.marco79423.net/articles/', articles)
+  store.dispatch(actions.fetchArticles())
+    .then(() => {
+      expect(store.getActions()).toEqual(expectedActions)
+      done()
+    })
 })
--- src/common/ducks/article/reducer.spec.js
+++ src/common/ducks/article/reducer.spec.js
@@ -3,18 +3,16 @@ import * as Immutable from 'immutable'
 import * as actions from './actions'
 import reducer from './reducer'
 
-describe('article.reducer', () => {
-  test('handle setArticles', () => {
-    const state = Immutable.fromJS({
-      items: []
-    })
-    const action = actions.setArticles([
-      'article 1', 'article2'
-    ])
-    const expectedNextState = Immutable.fromJS({
-      items: ['article 1', 'article2']
-    })
-
-    expect(reducer(state, action)).toEqual(expectedNextState)
+test('article reducer return a state with new articles', () => {
+  const state = Immutable.fromJS({
+    items: []
   })
+  const action = actions.setArticles([
+    'article 1', 'article2'
+  ])
+  const expectedNextState = Immutable.fromJS({
+    items: ['article 1', 'article2']
+  })
+
+  expect(reducer(state, action)).toEqual(expectedNextState)
 })
