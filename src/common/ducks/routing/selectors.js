e607685abeb1e45a3dd97c697fb031d8bcc43d1a
marco79423 <marco79423@gmail.com>
2017-04-08 14:16:38 +0800

    Changed the way to import lodash

--- src/common/ducks/routing/selectors.js
+++ src/common/ducks/routing/selectors.js
@@ -1,9 +1,9 @@
-import _ from 'lodash'
+import get from 'lodash/get'
 import { createSelector } from 'reselect'
 
 import * as configSelectors from '../config/selectors'
 
-export const getPathName = (state) => _.get(state.getIn(['routing', 'locationBeforeTransitions']), 'pathname') || '/'
+export const getPathName = (state) => get(state.getIn(['routing', 'locationBeforeTransitions']), 'pathname') || '/'
 export const getCurrentUrl = createSelector(
   [
     configSelectors.getHostUrl,
