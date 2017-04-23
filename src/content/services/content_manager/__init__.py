2e78faa6695d731c74523078f3765a734b257377
marco79423 <marco79423@gmail.com>
2017-04-23 20:27:21 +0800

    Fixed bug that would cause 404 error when user was trying to download files from site

--- src/content/services/content_manager/__init__.py
+++ src/content/services/content_manager/__init__.py
@@ -6,15 +6,15 @@ from django.core.files import File
 
 from content import entities
 from content import store
-from content.models import Article, Category, WebPage, AppFile
+from content.models import AppFile
 from content.services.content_manager.content_spider import ContentSpider
 from mysite_backend import settings
 
 
 class ContentManager:
     STATIC_IMAGE_URL = settings.HOST + settings.MEDIA_URL + "images/"
-    STATIC_IMAGE_DIR = settings.MEDIA_ROOT / "images"
 
+    MEDIA_IMAGE_DIR = settings.MEDIA_ROOT / "images"
     MEDIA_APPFILE_DIR = settings.MEDIA_ROOT / "appfiles"
 
     def __init__(self):
@@ -24,7 +24,7 @@ class ContentManager:
         store.ArticleStore.clean()
         store.WebPageStore.clean()
 
-        self.STATIC_IMAGE_DIR.rmtree_p()
+        self.MEDIA_IMAGE_DIR.rmtree_p()
         self.MEDIA_APPFILE_DIR.rmtree_p()
 
     def build(self, source_dir):
@@ -71,7 +71,7 @@ class ContentManager:
     def _setup_item_images(self, content, item_data):
         for item_image in item_data.item_images:
             _, _, basename = item_image['link'].rpartition("/")
-            target_dir = self.STATIC_IMAGE_DIR / slugify.slugify(item_data.title)
+            target_dir = self.MEDIA_IMAGE_DIR / slugify.slugify(item_data.title)
             self._save_optimized_images(item_image['path'], target_dir)
 
             content = content.replace(item_image['link'],
@@ -86,7 +86,7 @@ class ContentManager:
                 app_file.file.save(name=basename, content=File(fp))
             app_file.save()
 
-            content = content.replace(item_file['link'], "/files/" + app_file.slug + "/")
+            content = content.replace(item_file['link'], settings.HOST + "/files/" + app_file.slug + "/")
         return content
 
     @staticmethod
