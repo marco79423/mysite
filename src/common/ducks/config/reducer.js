5b2765070d87d8b726563351e850944107aa6f58
marco79423 <marco79423@gmail.com>
2017-06-07 23:36:20 +0800

    Revised code

--- src/common/ducks/config/reducer.js
+++ src/common/ducks/config/reducer.js
@@ -1,5 +1,5 @@
 import * as Immutable from 'immutable'
-import * as clientConfig from '../../../config/client'
+import * as clientConfig from '../../config'
 
 const defaultState = Immutable.fromJS(clientConfig)
 
