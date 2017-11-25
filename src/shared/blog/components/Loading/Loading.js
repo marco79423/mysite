a8e439824d8e1b43961221aa112336c38b69ea34
marco79423 <marco79423@gmail.com>
2017-11-25 18:49:26 +0800

    Use styled-components in Loading

--- src/shared/blog/components/Loading/Loading.js
+++ src/shared/blog/components/Loading/Loading.js
@@ -1,15 +1,42 @@
 import React from 'react'
-import classNames from 'classnames'
+import styled from 'styled-components'
+import twisterStyle from '../base/animation/twisterStyle'
+import loadingImage from './img/loading.jpg'
 
-import styles from './Loading.scss'
+const Base = styled.div`
+  margin-top: 10%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+
+const LoadingImage = styled.div`
+  display: inline-block;
+
+  width: 90px;
+  height: 90px;
+  background: url(${loadingImage}) no-repeat center;
+  background-size: 90px 90px;
+
+  border: 0.1em solid #FFFFFF;
+  box-shadow: 0 1px 4px rgba(0, 0, 0, 0.15);
+  
+  ${twisterStyle}
+`
+
+const Message = styled.div`
+  margin-top: 2rem;
+  font-size: 2rem;
+  text-align: center;
+`
 
 export default class Loading extends React.PureComponent {
   render () {
     return (
-      <div className={styles.root}>
-        <div className={classNames(styles.loadingImage, styles.twister)}/>
-        <div className={styles.message}>正在努力讀取中…</div>
-      </div>
+      <Base>
+        <LoadingImage/>
+        <Message>正在努力讀取中…</Message>
+      </Base>
     )
   }
 }
