878316a9a967918d6af576a87c071a2de476498e
marco79423 <marco79423@gmail.com>
2017-11-26 11:40:13 +0800

    Fix props warning in LinkButton

--- src/shared/blog/components/base/LinkButton.js
+++ src/shared/blog/components/base/LinkButton.js
@@ -1,7 +1,8 @@
+import React from 'react'
 import styled from 'styled-components'
 import NormalLink from './NormalLink'
 
-export default styled(NormalLink)`
+export default styled(({active, ...rest}) => <NormalLink {...rest} />)`
   color: #ECECEC;
   background: ${props => props.active ? '#065a76' : '#388ca8'};
   pointer-events: ${props => props.active ? 'none' : 'auto'};
