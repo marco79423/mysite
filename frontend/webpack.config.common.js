196b8d82082688d87e330595d7d7159c777e9c19
marco79423 <marco79423@gmail.com>
2018-06-16 20:12:41 +0800

    Make BACKEND_SERVER_URL, SITE_VERSION, and SITE_UPDATED_TIME able to get value from ENV

--- frontend/webpack.config.common.js
+++ frontend/webpack.config.common.js
@@ -7,7 +7,10 @@ const plugins = [
   new webpack.DefinePlugin({
     'process.env': {
       NODE_ENV: JSON.stringify(process.env.NODE_ENV),
-      DEBUG: DEBUG
+      DEBUG: DEBUG,
+      BACKEND_SERVER_URL: JSON.stringify(process.env.BACKEND_SERVER_URL),
+      SITE_VERSION: JSON.stringify(process.env.SITE_VERSION),
+      SITE_UPDATED_TIME: JSON.stringify(process.env.SITE_UPDATED_TIME)
     }
   })
 ]
