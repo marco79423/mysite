c8876535be5639488c3c92994152f2422dd53edc
marco79423 <marco79423@gmail.com>
2018-06-16 21:06:09 +0800

    Let frontend-express to be a docker container

--- contl.py
+++ contl.py
@@ -1,11 +1,9 @@
 import json
-import click
-import datetime as dt
 
+import click
 from fabric.api import *
 from fabric.colors import cyan
 from fabric.contrib.files import exists, sed
-
 
 PROJECT_CONFIG_FILE = "project_config.json"
 
@@ -45,18 +43,14 @@ def upload_proj(project_path, branch):
 
 
 @task
-def install_frontend_required_pkgs():
-    if not exists('/usr/bin/node'):
-        print(cyan('Install node and associated packages...'))
-        sudo('curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -')
+def setup_frontend_serv(project_path):
+    with cd(project_path):
+        sudo('docker-compose pull')
 
-    if not exists('/usr/bin/yarn'):
-        sudo('curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -')
-        sudo('echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list')
-        sudo('sudo apt-get update && sudo apt-get install yarn')
-
-    print(cyan('Install http server ...'))
-    sudo('apt-get install -y nginx')
+@task
+def restart_frontend_serv(project_path):
+    with cd(project_path):
+        sudo('docker-compose up --force-recreate --detach')
 
 
 @task
@@ -83,39 +77,6 @@ def install_backend_required_pkgs():
 
     print(cyan('Install http server ...'))
     sudo('apt-get install -y nginx')
-
-
-@task
-def setup_frontend_proj(frontend_path, project_version, server_name):
-    updated_time = str(dt.datetime.now())
-
-    with cd(frontend_path):
-        print(cyan('Prepare project ...'))
-        settings_path = '{}src/config/shared.js'.format(frontend_path)
-        sed(settings_path, 'BACKEND_SERVER_URL = ""', 'BACKEND_SERVER_URL = "https://{}"'.format(server_name), shell=True, use_sudo=True)
-        sed(settings_path, 'SITE_VERSION = ""', 'SITE_VERSION = "{}"'.format(project_version), shell=True, use_sudo=True)
-        sed(settings_path, 'SITE_UPDATED_TIME = ""', 'SITE_UPDATED_TIME = "{}"'.format(updated_time), shell=True, use_sudo=True)
-
-        sudo('yarn', warn_only=True)
-        sudo("yarn test")
-
-        sudo('yarn dist')
-        sudo('chown -R www-data:www-data .')
-
-
-@task
-def setup_frontend_serv(frontend_path, project_name, frontend_port):
-    print(cyan('Setup express ...'))
-    config_path = '/etc/init/{}-express.conf'.format(project_name)
-    with cd(frontend_path):
-        sudo('cp conf/init/service.conf {}'.format(config_path))
-        sed(config_path, 'TARGET_NAME', project_name, shell=True, use_sudo=True)
-        sed(config_path, 'PORT', str(frontend_port), shell=True, use_sudo=True)
-
-
-@task
-def restart_frontend_service(project_name):
-    sudo('service {}-express restart'.format(project_name))
 
 
 @task
@@ -222,22 +183,20 @@ def deploy(target, branch, prod):
     project_path = '/var/www/{}/'.format(config['name'])
     execute(upload_proj, project_path, branch)
 
-    commit_hash = local('git rev-parse {}'.format(branch), capture=True)
-    project_version = "{} ({})".format(branch, commit_hash)
-
     project_name = config['name']
     server_name = config['server_name']
     frontend_port = config['frontend_port']
-    backend_secret_key = env.backend_secret_key
 
     if target in ('frontend', 'all'):
-        execute(install_frontend_required_pkgs)
-        frontend_path = '{}frontend/'.format(project_path)
-        execute(setup_frontend_proj, frontend_path, project_version, server_name)
-        execute(setup_frontend_serv, frontend_path, project_name, frontend_port)
-        execute(restart_frontend_service, project_name)
+        execute(setup_frontend_serv, project_path)
+        execute(restart_frontend_serv, project_path)
 
     if target in ('backend', 'all'):
+        commit_hash = local('git rev-parse {}'.format(branch), capture=True)
+        project_version = "{} ({})".format(branch, commit_hash)
+
+        backend_secret_key = env.backend_secret_key
+
         execute(install_backend_required_pkgs)
 
         backend_path = '{}backend/'.format(project_path)
