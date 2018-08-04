e15a6e1b06b615eea3e7137e2f95bd39f807eb00
兩大類 <marco79423@gmail.com>
2018-08-04 20:35:47 +0800

    Remove unused API

--- backend/src/mysite_backend/urls.py
+++ backend/src/mysite_backend/urls.py
@@ -1,17 +1,12 @@
 from django.conf.urls import include
 from django.conf.urls import url
 from django.conf.urls.static import static
-from django.views.generic import TemplateView
-from rest_framework.authtoken import views as authtoken_views
 
 from blog import urls as api_urls
 from mysite_backend import settings
 
 urlpatterns = [
     url(r'^api/', include(api_urls)),
-    url(r'^auth/login/$', authtoken_views.obtain_auth_token),
-
-    url(r'^robots\.txt$', TemplateView.as_view(template_name='robots.txt')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 handler404 = 'views.get_article_list_page'
