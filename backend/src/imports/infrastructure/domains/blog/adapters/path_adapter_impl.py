2ce2b0cf4318ac98bc446bd2d900eb1a277900da
marco79423 <marco79423@gmail.com>
2018-12-29 14:39:07 +0800

    Add PathHandler

--- backend/src/imports/infrastructure/domains/blog/adapters/path_adapter_impl.py
+++ backend/src/imports/infrastructure/domains/blog/adapters/path_adapter_impl.py
@@ -1,23 +1,26 @@
+import injector
 import os
 from pathlib import Path
 
 from imports.domains.blog.adapters import PathAdapter
+from imports.infrastructure.base.path_handler import PathHandler
 
 
+@injector.singleton
 class PathAdapterImpl(PathAdapter):
 
+    @injector.inject
+    def __init__(self, path_handler: PathHandler):
+        self.path_handler = path_handler
+
     def exists(self, file_path: str) -> bool:
-        return Path(file_path).exists()
+        return self.path_handler.exists(file_path)
 
     def get_base_name(self, file_path: str) -> str:
-        return Path(file_path).name
+        return self.path_handler.get_base_name(file_path)
 
     def join(self, *file_paths: [str]) -> str:
-        return os.path.join(*file_paths)
+        return self.path_handler.join(*file_paths)
 
     def get_all_sub_folders(self, file_path: str) -> [str]:
-        file_paths = []
-        for sub_folder in Path(file_path).iterdir():
-            if sub_folder.is_dir():
-                file_paths.append(sub_folder)
-        return file_paths
+        return self.path_handler.get_all_sub_folders(file_path)
