98e11e8ffd9fdfe60ab2649d8fece06d7f8c32df
marco79423 <marco79423@gmail.com>
2019-01-07 20:14:12 +0800

    Remove onUpdate in router

--- frontend/src/imports/app/Root.js
+++ frontend/src/imports/app/Root.js
@@ -1,10 +1,16 @@
 import React from 'react'
-import { RouterContext } from 'react-router'
-import { Provider } from 'react-redux'
+import {Route, Router} from 'react-router'
+import {Provider} from 'react-redux'
 import PropTypes from 'prop-types'
-import { createRoutes } from './routes'
-
 import 'isomorphic-fetch'
+
+import ArticleListPage from './blog/pages/ArticleListPage'
+import LabPage from './blog/pages/LabPage'
+import SiteInfoPage from './blog/pages/SiteInfoPage'
+import CategorizedArticleListPage from './blog/pages/CategorizedArticleListPage'
+import ArchivesPage from './blog/pages/ArchivesPage'
+import ArticleDetailPage from './blog/pages/ArticleDetailPage'
+import PagePage from './blog/pages/PagePage'
 
 export default class Root extends React.Component {
   static PropTypes = {
@@ -13,10 +19,26 @@ export default class Root extends React.Component {
     renderProps: PropTypes.any,
   }
 
-  render () {
+  componentDidMount() {
+    this.props.history.listen((location, action) => {
+      window.scrollTo(0, 0)
+    })
+  }
+
+  render() {
     return (
       <Provider store={this.props.store}>
-        {createRoutes(this.props.history)}
+        <Router history={this.props.history}>
+          <Route path='/' component={ArticleListPage}/>
+          <Route path='/lab/' component={LabPage}/>
+          <Route path='/info/' component={SiteInfoPage}/>
+          <Route path='/articles/page/:pageNum/' component={ArticleListPage}/>
+          <Route path='/articles/category/:category/' component={CategorizedArticleListPage}/>
+          <Route path='/articles/category/:category/page/:pageNum/' component={CategorizedArticleListPage}/>
+          <Route path='/articles/archives/' component={ArchivesPage}/>
+          <Route path='/articles/:slug/' component={ArticleDetailPage}/>
+          <Route path='/:app/:slug/' component={PagePage}/>
+        </Router>
       </Provider>
     )
   }
