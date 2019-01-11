ecf9c837f8341ed94fe27892f1c312f3995ffaa3
marco79423 <marco79423@gmail.com>
2019-01-11 15:46:52 +0800

    Add RSS support

--- frontend/server/index.js
+++ frontend/server/index.js
@@ -1,10 +1,40 @@
 import path from 'path'
 import express from 'express'
+import {Feed} from 'feed'
+import axios from 'axios'
+
+import * as config from '../src/imports/config'
+
 
 
 const PORT = 3000
 
 const app = express()
+
+app.get('/rss.xml', function (req, res) {
+  return axios.get(`${config.BACKEND_SERVER_URL}/api/articles/`)
+    .then(res => res.data)
+    .then(articles => {
+      const feed = new Feed({
+        ...config.FEED_TEMPLATE,
+        updated: new Date(articles[0].date),
+      })
+      articles.forEach(article => {
+        feed.addItem({
+          title: article.title,
+          id: article.slug,
+          link: `${config.HOST_URL}/articles/${article.slug}/`,
+          description: article.rawSummary,
+          content: article.content,
+          author: [
+            config.FEED_TEMPLATE.author
+          ],
+          date: new Date(article.date),
+        })
+      })
+      res.send(feed.rss2())
+    })
+})
 
 app.use(express.static(
   path.resolve(__dirname, '..', 'build'),
