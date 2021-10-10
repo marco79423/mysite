cb00ff38693c25920e57c9e93903e3a78ad91b9e
Marco Li <marco_li@paradise-soft.com.tw>
2021-10-10 18:40:45 +0800

    修正分類文章列表不同頁的錯誤

--- frontend/pages/articles/category/[category]/page/[pageNum].js
+++ frontend/pages/articles/category/[category]/page/[pageNum].js
@@ -1,6 +1,6 @@
-import {wrapper} from '../../../../src/imports/ui/store'
-import AppLayoutContainer from '../../../../src/imports/ui/blog/containers/AppLayoutContainer'
-import ArticleListContainer from '../../../../src/imports/ui/blog/containers/ArticleListContainer'
+import {wrapper} from '../../../../../src/imports/ui/store'
+import AppLayoutContainer from '../../../../../src/imports/ui/blog/containers/AppLayoutContainer'
+import ArticleListContainer from '../../../../../src/imports/ui/blog/containers/ArticleListContainer'
 
 
 export const getServerSideProps = wrapper.getServerSideProps((store) => ({params}) => {
