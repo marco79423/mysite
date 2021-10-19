6dfc3c364c8baa63988751bcc59c5b7ef62172a4
marco79423 <marco79423@gmail.com>
2021-10-19 22:00:51 +0800

    移除部分不必要的設定

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,15 +1,5 @@
 module.exports = {
-  experimental: {
-    craCompat: true,
-  },
-
   publicRuntimeConfig: require('./runtimeConfig'),
-
-  // Remove this to leverage Next.js' static image handling
-  // read more here: https://nextjs.org/docs/api-reference/next/image
-  images: {
-    disableStaticImages: true
-  },
 
   eslint: {
     // Warning: This allows production builds to successfully complete even if
