b431ce1f2d7214d1ae661a6b66f53ba94bfd6405
marco79423 <marco79423@gmail.com>
2022-04-14 00:57:50 +0800

    升為 v1.2.0

--- frontend/runtimeConfig.js
+++ frontend/runtimeConfig.js
@@ -1,6 +1,6 @@
 module.exports = {
   GtagTrackerID: 'UA-38552387-1',
 
-  // BackendServerUrl: 'https://marco79423.net/backend',
-  BackendServerUrl: 'http://localhost:8000/backend',
+  BackendServerUrl: 'https://marco79423.net/backend',
+  // BackendServerUrl: 'http://localhost:8000/backend',
 }
