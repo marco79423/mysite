cd84c211377b6c09667e0684f7e7858ce1734054
Marco Li <marco_li@paradise-soft.com.tw>
2022-05-05 18:28:58 +0800

    feat: 更新版本

--- frontend/components/elements/generic/Article/Article.js
+++ frontend/components/elements/generic/Article/Article.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
-import {LikeCoinButton} from '@paji-sdk/browser'
+import {LikeCoinButton} from '@paji-sdk/web'
 
 import TitleLink from '../TitleLink'
 import RstContent from '../RstContent'
