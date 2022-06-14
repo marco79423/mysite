a0c4b167c7dce7bede2d2d951daf7947bfaee263
Marco Li <marco_li@paradise-soft.com.tw>
2022-06-14 13:41:03 +0800

    chore: 更新套件版本

--- backend/src/imports/infrastructure/domains/blog/model/asset_model.py
+++ backend/src/imports/infrastructure/domains/blog/model/asset_model.py
@@ -7,4 +7,4 @@ class AssetModel(db.Model):
     uuid = db.Column(db.String(64), primary_key=True)
     is_attachment = db.Column(db.Boolean, default=False, nullable=False)
     filename = db.Column(db.String(120), nullable=False)
-    data = db.Column(db.Binary, nullable=False)
+    data = db.Column(db.LargeBinary, nullable=False)
