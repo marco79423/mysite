c453701a009d8e8b73c9515d4b29e57d2de47803
marco79423 <marco79423@gmail.com>
2023-02-07 21:13:40 +0800

    feat: support mdx

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,8 +1,29 @@
+const withMDX = require('@next/mdx')
+
 /** @type {import('next').NextConfig} */
-module.exports = {
+const nextConfig = {
+  reactStrictMode: true,
+
   publicRuntimeConfig: require('./runtimeConfig'),
+
+  // Configure pageExtensions to include md and mdx
+  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
 
   compiler: {
     emotion: true,
   },
 }
+
+const plugins = [
+  withMDX({
+    // If you use remark-gfm, you'll need to use next.config.mjs
+    // as the package is ESM only
+    // https://github.com/remarkjs/remark-gfm#install
+    remarkPlugins: [],
+    rehypePlugins: [],
+    // If you use `MDXProvider`, uncomment the following line.
+    // providerImportSource: "@mdx-js/react",
+  })
+]
+
+module.exports = () => plugins.reduce((acc, next) => next(acc), nextConfig)
