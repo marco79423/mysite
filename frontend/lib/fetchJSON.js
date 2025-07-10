75dc43879a1a99e768eda60fc9833ca91acc73f1
兩大類 <marco79423@gmail.com>
2025-07-10 20:46:32 +0800

    chore: remove debug console log from fetchJSON function

--- frontend/lib/fetchJSON.js
+++ frontend/lib/fetchJSON.js
@@ -4,7 +4,6 @@ const dns = require('dns')
 dns.setDefaultResultOrder('ipv4first')
 
 export default function fetchJSON(url) {
-  console.log(encodeURI(url))
   return axios.get(encodeURI(url))
     .then(response => response.data)
 }
